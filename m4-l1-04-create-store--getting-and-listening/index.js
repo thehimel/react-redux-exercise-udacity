dc5ad1cdccd096e28ef9ccc1fd0c42d9e0c202ac
Himel Das <himeldas66@gmail.com>
2020-05-04 18:38:15 +0600

    refactor

---
+++ m4-l1-04-create-store--getting-and-listening/index.js
@@ -0,0 +1,17 @@
+function createStore () {
+    /*
+    The Store should have 4 parts:
+    1. The state
+    2. Get the state
+    3. Listen to changes on state
+    4. Update the state
+    */
+
+    let state;
+
+    const getState = () => state;
+
+    return {
+        getState();
+    }
+}

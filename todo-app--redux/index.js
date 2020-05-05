47e15ef69895f757288464e2dab54a50e5f52473
Himel Das <himeldas66@gmail.com>
2020-05-05 18:32:22 +0600

    Create index.js

---
+++ todo-app--redux/index.js
@@ -0,0 +1,27 @@
+function createStore () {
+    /*
+    The Store should have 4 parts:
+    1. The state
+    2. Get the state
+    3. Listen to changes on state
+    4. Update the state
+    */
+
+    let state;
+    let listeners = [];
+
+    const getState = () => state;
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter((l) => l !== listener);
+        }
+    };
+
+
+    return {
+        getState,
+        subscribe
+    }
+}
